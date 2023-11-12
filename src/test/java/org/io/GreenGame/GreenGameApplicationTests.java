c5ff86cfc7a7501615ee9dcb37c5b6e2f863765f
Tomunek <kowalczyk.tomek11@gmail.com>
2023-11-12 13:42:57 +0100

    Created empty Spring application

---
+++ src/test/java/org/io/GreenGame/GreenGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.io.GreenGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
