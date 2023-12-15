e12c7c258d279147975be8fcf0910b3f94bfa026
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-12-15 13:29:49 +0100

    Added simple layout

---
+++ fight-module/src/components/FightEnvironment.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="env">
+    <div id="player">
+      <div id="playerBody">
+
+      </div>
+      <div id="playerHead">
+
+      </div>
+      <div id="playerWeapon">
+
+      </div>
+    </div>
+    <div id="enemy">
+      <div id="enemyBody">
+
+      </div>
+      <div id="enemyHead">
+
+      </div>
+      <div id="enemyWeapon">
+
+      </div>
+    </div>
+    <div id="HP">
+        <h1>HP</h1>
+        <div id="healthBar">
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FightEnvironment"
+}
+</script>
+
+<style scoped>
+template {
+  display: flex;
+}
+
+#env {
+  width: 100%;
+  height: 737px;
+  background-image: url('~@/assets/forest.jpg');
+  background-size: cover;
+  background-attachment: fixed;
+}
+
+#player {
+  position: absolute;
+  bottom: 10%;
+  left: 25%;
+  width: 20%;
+  height: 30%;
+}
+
+#enemy {
+  position: absolute;
+  bottom: 10%;
+  left: 60%;
+  width: 20%;
+  height: 30%;
+}
+
+#playerBody {
+  background-image: url("~@/assets/body.png");
+  position: absolute;
+  width: 75%;
+  height: 100%;
+}
+
+#enemyBody {
+  background-image: url("~@/assets/body.png");
+  position: absolute;
+  width: 75%;
+  height: 100%;
+}
+
+#playerHead {
+  background-image: url("~@/assets/stud00.png");
+  position: absolute;
+  bottom: 50%;
+  width: 50%;
+  left: 17%;
+  height: 65%;
+}
+
+#enemyHead {
+  background-image: url("~@/assets/stud11.png");
+  position: absolute;
+  bottom: 53%;
+  width: 50%;
+  left: 17%;
+  height: 65%;
+}
+
+#playerWeapon {
+  background-image: url("~@/assets/brick.png");
+  position: absolute;
+  bottom: 35%;
+  left: -17%;
+  width: 100%;
+  height: 100%;
+}
+
+#HP{
+  position: absolute;
+  display: flex;
+  margin-left: 1%;
+  text-align: center;
+  color: crimson;
+  width: 30%;
+  height: 20%;
+  flex-direction: column;
+}
+
+#healthBar{
+  position: relative;
+  border: solid antiquewhite;
+  height: 25%;
+  width: 100%;
+}
+
+</style>
