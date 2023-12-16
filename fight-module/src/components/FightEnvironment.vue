6d3cbc58ea75e82691872d288db088d0c8a6d7b5
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-12-16 12:56:21 +0100

    Added resizable health bar

--- fight-module/src/components/FightEnvironment.vue
+++ fight-module/src/components/FightEnvironment.vue
@@ -11,7 +11,7 @@
 
       </div>
     </div>
-    <div id="enemy">
+    <div id="enemy" v-on:click="showQuestions">
       <div id="enemyBody">
 
       </div>
@@ -23,18 +23,32 @@
       </div>
     </div>
     <div id="HP">
-        <h1>HP</h1>
-        <div id="healthBar">
-
-        </div>
+      <h1>HP</h1>
+      <div id="healthBar" :style="{width: this.HPBar + '%'}">
+      </div>
+    </div>
+    <div id="score">
+      <h1>Score: {{ this.score }}</h1>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FightEnvironment"
-}
+  name: "FightEnvironment",
+  data() {
+    return {
+      HPBar: 100,
+      score: 0,
+    };
+  },
+  methods:
+      {
+        showQuestions() {
+          this.emitter.emit('showQuestions')
+        }
+      }
+};
 </script>
 
 <style scoped>
@@ -92,7 +106,7 @@ template {
 #enemyHead {
   background-image: url("~@/assets/stud11.png");
   position: absolute;
-  bottom: 53%;
+  bottom: 50%;
   width: 50%;
   left: 17%;
   height: 65%;
@@ -107,7 +121,7 @@ template {
   height: 100%;
 }
 
-#HP{
+#HP {
   position: absolute;
   display: flex;
   margin-left: 1%;
@@ -118,11 +132,20 @@ template {
   flex-direction: column;
 }
 
-#healthBar{
+#healthBar {
   position: relative;
-  border: solid antiquewhite;
+  background-color: crimson;
+  border-radius: 15px;
   height: 25%;
-  width: 100%;
+}
+
+#score {
+  display: flex;
+  position: absolute;
+  color: antiquewhite;
+  text-align: start;
+  margin-top: 9%;
+  margin-left: 1%;
 }
 
 </style>
--- fight-module/src/components/QuizQuestion.vue
+++ fight-module/src/components/QuizQuestion.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-
+  <div id="pop-up" v-if="this.showPopup">
+    <h1>{{this.question.question}}</h1>
   </div>
 </template>
 
 <script>
+import json from '../../public/question.json'
 export default {
-  name: "QuizQuestion"
+  name: "QuizQuestion",
+  data() {
+    return {
+      showPopup: false,
+      question: json
+    };
+  },
+  mounted() {
+    this.emitter.on('showQuestions', () => {
+        this.showPopup = true;
+    })
+  }
 }
 </script>
 
 <style scoped>
+#pop-up{
+  top: 20%;
+  left: 15%;
+  position: fixed;
+  margin: auto;
+  display: flex;
+  height: 75%;
+  width: 75%;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+}
 
 </style>
