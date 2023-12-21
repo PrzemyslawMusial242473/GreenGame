1c95cc62435adc9cb535824abc2d81011992ec70
Piotr Skrobski <piotrskrobski2001@protonmail.com>
2023-12-21 18:28:27 +0100

    Wrote initial structure for Chat module. No frontend.

--- backend/src/main/java/org/io/GreenGame/user/repository/FriendModelRepository.java
+++
@@ -1,7 +0,0 @@
-package org.io.GreenGame.user.repository;
-
-import org.io.GreenGame.user.model.FriendModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendModelRepository extends JpaRepository<FriendModel, Long> {
-}
--- backend/src/main/java/org/io/GreenGame/user/service/strategy/SortingStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.io.GreenGame.user.service.strategy;
-
-import org.io.GreenGame.user.model.FriendModel;
-
-import java.util.List;
-
-public interface SortingStrategy {
-    List<FriendModel> apply(List<FriendModel> friends);
-}
