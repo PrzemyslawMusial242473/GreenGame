30bd7b54163f9983458f70ad0e0123557d37d644
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-01-17 12:04:48 +0100

    Merged with master

---
+++ backend/src/main/java/org/io/GreenGame/fight/FightApplication.java
@@ -0,0 +1,13 @@
+package org.io.GreenGame.fight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightApplication.class, args);
+	}
+
+}
