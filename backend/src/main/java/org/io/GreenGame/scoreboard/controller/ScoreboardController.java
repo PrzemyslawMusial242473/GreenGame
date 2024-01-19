9c7c0198eb870e9208b1b2c03d07b788411656dc
Adam Stefański <adamtonki@gmail.com>
2024-01-19 22:30:35 +0100

    Working version

--- backend/src/main/java/org/io/GreenGame/scoreboard/controller/ScoreboardController.java
+++ backend/src/main/java/org/io/GreenGame/scoreboard/controller/ScoreboardController.java
@@ -33,33 +33,17 @@ public class ScoreboardController {
     public List<ScoreReturnDto> getScoreboard() {
         List<Score> scores = scoreboardService.getAllScores();
         return mapToScoreReturnDto(scores);
-//        List<ScoreReturnDto> returnDtos = new ArrayList<>();
-//        for (int i = 1; i <= scores.size(); i++) {
-//            returnDtos.add(new ScoreReturnDto(i,
-//                    scores.get(i - 1).getUser().getUsername(),
-//                    scores.get(i - 1).getPoints(),
-//                    scores.get(i - 1).getNumberOfGames(),
-//                    scores.get(i - 1).getAchievements()
-//                            .stream()
-//                            .map(achievement -> new AchievementDto(achievement.getAchievementName())).toList()));
-//        }
-//        return returnDtos;
-//        return scores.stream().map(score -> new ScoreReturnDto(score.getRank(),
-//                score.getUser().getUsername(),
-//                score.getPoints(),
-//                score.getNumberOfGames(),
-//                score.getAchievements().stream().map(achievement -> new AchievementDto(achievement.getAchievementName())).toList())).toList();
     }
 
     @PostMapping("/scoreboard")
     @ResponseBody
     public ResponseEntity<Integer> addScore(@RequestBody ScoreReqDto scoreReqDto) {
         try {
-            int points = scoreboardService.addPointsToUser(getIdOfLoggedUser(),
+            int rank = scoreboardService.addPointsToUser(getIdOfLoggedUser(),
                     scoreReqDto.getPoints(),
                     scoreReqDto.getNumberOfQuestions(),
                     scoreReqDto.getHp());
-            return ResponseEntity.ok(points);
+            return ResponseEntity.ok(rank);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
@@ -72,17 +56,6 @@ public class ScoreboardController {
         List<Score> scores = scoreboardService.getTopScores(limit);
         System.out.println("TOP SCORES: " + scores.size());
         return mapToScoreReturnDto(scores);
-//        List<ScoreReturnDto> returnDtos = new ArrayList<>();
-//        for (int i = 1; i <= scores.size(); i++) {
-//            returnDtos.add(new ScoreReturnDto(i,
-//                    scores.get(i - 1).getUser().getUsername(),
-//                    scores.get(i - 1).getPoints(),
-//                    scores.get(i - 1).getNumberOfGames(),
-//                    scores.get(i - 1).getAchievements()
-//                            .stream()
-//                            .map(achievement -> new AchievementDto(achievement.getAchievementName())).toList()));
-//        }
-//        return returnDtos;
     }
 
     @GetMapping("/scoreboard/friends")
@@ -95,26 +68,11 @@ public class ScoreboardController {
         System.out.println("Score: " + score.getPoints());
 
         return mapToScoreReturnDto(scores);
-//        List<ScoreReturnDto> returnDtos = new ArrayList<>();
-//        for (int i = 1; i <= scores.size(); i++) {
-//            returnDtos.add(new ScoreReturnDto(i,
-//                    scores.get(i - 1).getUser().getUsername(),
-//                    scores.get(i - 1).getPoints(),
-//                    scores.get(i - 1).getNumberOfGames(),
-//                    scores.get(i - 1).getAchievements()
-//                            .stream()
-//                            .map(achievement -> new AchievementDto(achievement.getAchievementName())).toList()));
-//        }
-//        return returnDtos;
     }
 
     @GetMapping("/scoreboard/user")
     @ResponseBody
     public ScoreReturnDto getScoreByUser() {
-//        GreenGameUser user = authServiceImplementation.getUserFromSession();
-//        System.out.println("User: " + user.getEmail());
-//        Score score = scoreboardService.getScoreByEmail(user.getEmail());
-//        System.out.println("Score: " + score.getPoints());
         Score score = scoreboardService.getScoreByUserId(getIdOfLoggedUser());
         return new ScoreReturnDto(0,
                 score.getUser().getUsername(),
@@ -129,46 +87,9 @@ public class ScoreboardController {
         return scoreboardService.getScorePointsByUserId(getIdOfLoggedUser());
     }
 
-//    @GetMapping("/scoreboard/user")
-//    @ResponseBody
-//    public ScoreReturnDto getScoreByEmail(@RequestBody EmailDto email) {
-//        String userEmail = email.getEmail();
-//        System.out.println("USER EMAIL: " + userEmail);
-//        Score score = scoreboardService.getScoreByEmail(userEmail);
-//        System.out.println("SCORE: " + score.getUser().getEmail());
-//        return new ScoreReturnDto(score.getRank(),
-//                score.getUser().getUsername(),
-//                score.getPoints(),
-//                score.getNumberOfGames(),
-//                score.getAchievements().stream().map(achievement -> new AchievementDto(achievement.getAchievementName())).toList());
-//    }
-
-//    @GetMapping("/scoreboard/user/points")
-//    @ResponseBody
-//    public int getScorePointsByEmail(@RequestBody EmailDto email) {
-//        String userEmail = email.getEmail();
-//        System.out.println("USER EMAIL: " + userEmail);
-//        Score score = scoreboardService.getScoreByEmail(userEmail);
-//        System.out.println("SCORE: " + score.getUser().getEmail());
-//        return score.getPoints();
-//    }
-//
-//    @GetMapping("/scoreboard/user/rank")
-//    @ResponseBody
-//    public int getScoreRankByEmail(@RequestBody EmailDto email) {
-//        System.out.println("USER EMAIL: " + email.getEmail());
-//        String userEmail = email.getEmail();
-//        Score score = scoreboardService.getScoreByEmail(userEmail);
-//        System.out.println("SCORE: " + score.getUser().getEmail());
-//        return score.getRank();
-//    }
-
     @GetMapping("/scoreboard/user/achievement")
     @ResponseBody
     public List<Achievement> getUserAchievements() {
-//        String userEmail = email.getEmail();
-//        System.out.println("USER EMAIL: " + userEmail);
-//        Score score = scoreboardService.getScoreByEmail(userEmail);
         Score score = scoreboardService.getScoreByUserId(getIdOfLoggedUser());
         System.out.println("SCORE: " + score.getUser().getEmail());
         return score.getAchievements();
@@ -190,7 +111,6 @@ public class ScoreboardController {
     }
 
     @PostMapping("/scoreboard/achievement")
-//    @ResponseBody
     public ResponseEntity addAchievement(@RequestBody AchievementDto achievementDto) {
         return ResponseEntity.ok(scoreboardService.addAchievement(achievementDto.getAchievementName()));
     }
@@ -200,17 +120,6 @@ public class ScoreboardController {
     }
 
     private List<ScoreReturnDto> mapToScoreReturnDto(List<Score> scores) {
-//        List<ScoreReturnDto> returnDtos = new ArrayList<>();
-//        for (Score score : scores) {
-//            returnDtos.add(new ScoreReturnDto(
-//                    score.getUser().getUsername(),
-//                    score.getPoints(),
-//                    score.getNumberOfGames(),
-//                    score.getAchievements()
-//                            .stream()
-//                            .map(achievement -> new AchievementDto(achievement.getAchievementName())).toList()));
-//        }
-//        return returnDtos;
         List<ScoreReturnDto> returnDtos = new ArrayList<>();
         for (int i = 1; i <= scores.size(); i++) {
             returnDtos.add(new ScoreReturnDto(i,
